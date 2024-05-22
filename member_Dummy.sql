7f3eb7b97b793ce4a0fbb60ef13a87e2e8edf1cc
HyeokJin-Choi <126738953+HyeokJin-Choi@users.noreply.github.com>
2024-05-22 21:47:59 +0900

    Update and rename Dummy.sql to member_Dummy.sql

--- member_Dummy.sql
+++ member_Dummy.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
--- Host: localhost    Database: mydb
+-- Host: localhost    Database: poject1
 -- ------------------------------------------------------
 -- Server version	8.0.34
 
