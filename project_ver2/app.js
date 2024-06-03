d6efef0b528dfb3c2a522d384e427654f4edeafb
zozeyx <126737048+zozeyx@users.noreply.github.com>
2024-06-03 14:46:58 +0900

    Update app.js

--- project_ver2/app.js
+++ project_ver2/app.js
@@ -1,300 +1,250 @@
-//npm install express
-//npm install ejs
-//npm install body-parser
-//npm install express-session
-//npm install dotnev
-//npm install mysql2
-const express = require('express')
-const ejs = require('ejs') //js 코드를 html템플릿에 삽입하여 동적으로 웹페이지 생성
-const app = express()
-const path = require('path');
-const port = 3000
-var bodyParser = require('body-parser')
-var session = require('express-session')
+<head>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
 
-require('dotenv').config()
+<%- include('header') %>
 
-const mysql = require('mysql2')
-const connection = mysql.createConnection(process.env.DATABASE_URL)
-console.log("connected db")
-
-// 정적 파일 서빙 설정
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.set('view engine', 'ejs')
-app.set('views', './views') //views라는 폴더안에 있는걸 가져온다
-
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// Use the session middleware
-app.use(session({ secret: 'yesung', cookie: { maxAge: 60000 }, resave:true, saveUninitialized:true,}))
-
-app.use((req, res, next) => {
-
-  res.locals.user_id = "";
-  
-  if(req.session.member){
-    res.locals.user_id = req.session.member.user_id;
-  }
-  next()
-})
-
-//localhost:3000/ 를 입력하면 나온다
-//추가
-app.get('/', (req, res) => {
-  // 페이지 번호 가져오기
-  const page = parseInt(req.query.page) || 1;
-  const perPage = 9; // 페이지당 데이터 개수
-
-  // 페이지에 해당하는 데이터 가져오는 쿼리 수정
-  let offset = (page - 1) * perPage;
-  let sql = `
-  SELECT p.category, p.project_seq, p.introduce_title, p.teck_stack, p.position, p.deadline, m.nickname
-  FROM project_info p
-  JOIN member m ON p.user_id = m.user_id
-  LIMIT ${perPage}
-  OFFSET ${offset}`;
-
-  connection.query(sql, (err, results) => {
-      if (err) throw err;
-
-      // 전체 데이터 개수 가져오기 (페이징 처리를 위해)
-      let countSql = `SELECT COUNT(*) as count FROM project_info`;
-      connection.query(countSql, (err, countResult) => {
-          if (err) throw err;
-          const totalCount = countResult[0].count;
-
-          // 전체 페이지 수 계산
-          const totalPages = Math.ceil(totalCount / perPage);
-
-          // EJS 템플릿 렌더링
-          res.render('index', { projects: results, totalPages, currentPage: page });
-      });
-  });
-})
-
-app.get('/mypage', (req, res) => { //수정한 부분
-  if(req.session.member == null){
-    res.send("<script> alert('로그인하고 접근해주세요'); location.href='/';</script>")
-  }else{
-    const user_id = req.session.member.user_id;
-    const sql = 'SELECT * FROM member WHERE user_id = ?';
-
-    connection.query(sql, [user_id], (err, result) => {
-      if (err) throw err;
-      if (result.length === 0) {
-        res.send("<script> alert('회원 정보를 찾을 수 없습니다'); location.href='/';</script>");
-      } else {
-        res.render('mypage', { member: result[0] });
-      }
-    });
-  }
-});
-
-app.post('/mypageProc', (req, res) => {
-  const user_id = req.session.member.user_id; //수정한 부분
-  let nickname = req.body.nickname;
-  let job = req.body.job;
-  let affiliation = req.body.affiliation;
-  let career = req.body.career;
-  let introduce = req.body.introduce;
-  let interest_stack = req.body.interest_stack;
-
-  if (interest_stack && interest_stack !== null) {
-    interest_stack = interest_stack.toString();
-    interest_stack = interest_stack.replace(/'/g, '');
-  }
-  
-  if (!nickname) {
-    nickname = req.session.member.nickname;
-  }
-
-  if (!affiliation) {
-    affiliation = req.session.member.affiliation;
-  }
-
-  if (!career) {
-    career = req.session.member.career;
-  }
-
-  if (!introduce) {
-    introduce = req.session.member.introduce;
-  }
-
-
-  // 먼저 nickname 중복 확인
-  var checkSql = 'SELECT * FROM member WHERE nickname = ? AND user_id != ?';
-  connection.query(checkSql, [nickname,user_id], function (err, result) {
-    if (err) throw err;
-
-    if (result.length > 0) {
-      res.send("<script> alert('이미 존재하는 닉네임입니다. 다른 닉네임을 선택해주세요.'); history.back();</script>");
-    } else {
-  var sql = `
-    UPDATE member 
-    SET 
-      nickname = ?,
-      job = ?, 
-      affiliation = ?, 
-      career = ?, 
-      introduce = ?, 
-      interest_stack = ? 
-    WHERE 
-      user_id = ?
-  `;
-  var values = [nickname, job, affiliation, career, introduce, interest_stack, user_id];
-
-  connection.query(sql, values, function(err, result){
-    if(err) throw err;
-    console.log('자료 1개를 삽입하였습니다');
-    res.send("<script> alert('마이페이지가 수정되었습니다.'); location.href='/mypage';</script>")
-    });
-    }
-  });
-});
-
-app.get('/login', (req, res) => {
-  res.render('login')
-})
-
-app.post('/loginProc', (req, res) => {
-  const user_id = req.body.user_id;
-  const user_pw = req.body.user_pw;
-
-  var sql = `select * from member where user_id=? and user_pw=? `
-  var values = [user_id, user_pw]
-
-  connection.query(sql, values, function(err, result){
-    if(err) throw err;
-    if(result.length==0){
-      res.send("<script> alert('존재하지 않는 아이디입니다.'); location.href='/login';</script>")
-    } else{
-      req.session.member = result[0];
-      res.send("<script> location.href='/';</script>")
-    }
-  })
-})
-
-app.get('/logout', (req, res) => {
-  req.session.member = null;
-  res.send("<script> alert('로그아웃 되었습니다'); location.href='/';</script>")
-
-})
-
-app.get('/join', (req, res) => {
-  res.render('join')
-})
-
-app.post('/joinProc', (req, res) => {
-  const { user_id, user_pw, user_name, user_phone, nickname } = req.body;
-
-  // 먼저 user_id 중복 확인
-  var checkSql = 'SELECT * FROM member WHERE user_id = ?';
-  connection.query(checkSql, [user_id], function(err, result) {
-    if (err) throw err;
-
-    if (result.length > 0) {
-      res.render('join', {
-        user_id: user_id,
-        user_pw: user_pw,
-        user_name: user_name,
-        user_phone: user_phone,
-        nickname: nickname,
-        error: '이미 존재하는 아이디입니다.'
-      });
-    } else {
-      var insertSql = `INSERT INTO member (user_id, user_pw, user_name, user_phone, nickname) VALUES (?, ?, ?, ?, ?)`;
-      var values = [user_id, user_pw, user_name, user_phone, nickname];
-
-      connection.query(insertSql, values, function(err, result) {
-        if (err) throw err;
-
-        res.send("<script> alert('회원가입 성공하였습니다'); location.href='/';</script>");
-      });
-    }
-  })
-})
-
-//추가
-app.get('/board', (req, res) => {
-  // 페이지 번호 가져오기
-  const page = parseInt(req.query.page) || 1;
-  const perPage = 9; // 페이지당 데이터 개수
-
-  // 페이지에 해당하는 데이터 가져오는 쿼리 수정
-  let offset = (page - 1) * perPage;
-  let sql = `
-  SELECT p.category, p.project_seq, p.introduce_title, p.teck_stack, p.position, p.deadline, m.nickname
-  FROM project_info p
-  JOIN member m ON p.user_id = m.user_id
-  LIMIT ${perPage}
-  OFFSET ${offset}`;
-
-  connection.query(sql, (err, results) => {
-      if (err) throw err;
-
-      // 전체 데이터 개수 가져오기 (페이징 처리를 위해)
-      let countSql = `SELECT COUNT(*) as count FROM project_info`;
-      connection.query(countSql, (err, countResult) => {
-          if (err) throw err;
-          const totalCount = countResult[0].count;
-
-          // 전체 페이지 수 계산
-          const totalPages = Math.ceil(totalCount / perPage);
-
-          // EJS 템플릿 렌더링
-          res.render('board', { projects: results, totalPages, currentPage: page });
-      });
-  });
-})
-
-
-
-//추가
-app.get('/registration', (req, res) => {
-  if(req.session.member == null){
-    res.send("<script> alert('로그인하고 접근해주세요'); location.href='/';</script>")
-  }else{
-  res.render('registration')
-  }
-})
-
-//추가
-app.post('/registrationProc', (req, res) => {
-
-  const user_id = req.session.member.user_id; // 현재 세션의 사용자 ID 가져오기
-  const { category, mem_number, way, period, teck_stack, deadline, position, contact, introduce_title, introduce_detail } = req.body
-
-  var insertSql = `INSERT INTO project_info (category, mem_number, way, period, teck_stack, deadline, position, contact, introduce_title, introduce_detail, date, user_id) 
-  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, CURRENT_DATE(), ?)`
-  var values = [category, mem_number, way, period, teck_stack, deadline, position, contact, introduce_title, introduce_detail, user_id]
-
-  connection.query(insertSql, values, function (err, result) {
-    if (err) throw err
-    res.send("<script> alert('게시글이 등록되었습니다'); location.href='/';</script>")
-  })
-})
-
-//추가 (프로젝트 제목 클릭하면 그 게시물에 대한 페이지 열림)
-app.get('/project/:project_seq', (req, res) => {
-  const project_seq = req.params.project_seq;
-  
-  var selectSql = `SELECT * FROM project_info WHERE project_seq = ?`;
-  
-  connection.query(selectSql, [project_seq], function(err, result) {
-    if (err) throw err;
+<div class="main_text1">
+    <div class="top_project">
+        <span>추천 프로젝트</span>
+        <div class="turn_project"><img src="/images/좌측화살표.png">  <img src="/images/우측화살표.png"></div>
+    </div>
+    <ul class="icons">
+        <li>
+            <div class="icon_img">
+                <img src="/images/c.png">
+            </div>
+            <div class="hompage_top_contents1_bold">추천 프로젝트1</div>
+            <div class="hompage_top_contents1">추천 프로젝트1</div>	
+            <div class="more">추천 프로젝트1</div>		
+        </li>
+        <li>
+            <div class="icon_img">
+                <img src="/images/c++.png">
+            </div>
+            <div class="hompage_top_contents1_bold">추천 프로젝트2</div>
+            <div class="hompage_top_contents1">추천 프로젝트2</div>
+            <div class="more">추천 프로젝트2</div>
+        </li>
+        <li>
+            <div class="icon_img">
+                <img src="/images/cc.png">
+            </div>
+            <div class="hompage_top_contents1_bold">추천 프로젝트3</div>
+            <div class="hompage_top_contents1">추천 프로젝트3</div>
+            <div class="more">추천 프로젝트3</div>
+        </li>
+    </ul>
     
-    if (result.length > 0) {
-      console.log(result);
-      res.render('projectDetail', { projects: result[0] });
-    } else {
-      console.log('No project found');
-      res.send("<script> alert('게시물이 존재하지 않습니다'); location.href='/board';</script>");
-    }
-  });
-});
+    <div class="middle_project">
+        <button id="all_button">전체</button>&emsp;
+        <button id="popular_button">인기순</button>&emsp;
+        <button id="recent_button">최신순</button>
+    </div>
+    <form id="searchCategoryForm"> <!--카테고리 form-->
+    <div class="select_search">
+        <div class="select_category">
+            <select name="category">
+                <option selected disabled hidden>카테고리</option>
+                <option>프로젝트</option>
+                <option>스터디</option>
+            </select>
+        </div>
+        <div class="select_skill">
+            <select name="skill_stack">
+                <optgroup label="프론트엔드">
+                    <option selected disabled hidden>기술스택</option>
+                    <option value="JavaScript">JavaScript</option>
+                    <option value="TypeScript">TypeScript</option>
+                    <option value="React">React</option>
+                    <option value="Vue">Vue</option>
+                    <option value="Svelte">Svelte</option>
+                    <option value="Nextjs">Nextjs</option>
+                </optgroup>
+                <optgroup label="백엔드">
+                    <option value="Nodejs">Nodejs</option>
+                    <option value="Java">Java</option>
+                    <option value="Spring">Spring</option>
+                    <option value="Go">Go</option>
+                    <option value="Nestjs">Nestjs</option>
+                    <option value="Kotlin">Kotlin</option>
+                    <option value="Express">Express</option>
+                    <option value="MySQL">MySQL</option>
+                    <option value="MonggoDB">MonggoDB</option>
+                    <option value="Python">Python</option>
+                    <option value="Django">Django</option>
+                    <option value="php">php</option>
+                    <option value="GraphQL">GraphQL</option>
+                    <option value="Firebase">Firebase</option>
+                </optgroup>
+                <optgroup label="모바일">
+                    <option value="Flutter">Flutter</option>
+                    <option value="Swift">Swift</option>
+                    <option value="ReactNative">ReactNative</option>
+                    <option value="Unity">Unity</option>
+                </optgroup>
+                <optgroup label="기타">						
+                    <option value="AWS">AWS</option>
+                    <option value="Kubernetes">Kubernetes</option>
+                    <option value="Docker">Docker</option>
+                    <option value="Git">Git</option>
+                    <option value="Figma">Figma</option>
+                    <option value="Zeplin">Zeplin</option>
+                    <option value="Jest">Jest</option>
+                </optgroup>
+            </select>
+        </div>
+        <div class="select_roll">
+            <select name="roll">
+                <option selected disabled hidden>역할</option>
+                    <option value="backend">백엔드</option>
+                    <option value="frontend">프론트엔드</option>
+                    <option value="designer">디자이너</option>
+                    <option value="IOS">IOS</option>
+                    <option value="android">안드로이드</option>
+                    <option value="dev">데브옵스</option>
+                    <option value="PM">PM</option>
+                    <option value="planner">기획자</option>	
+                    <option value="marketer">마케터</option>
+            </select>
+        </div>
+                <div class="select_button">
+                    <button type="button" id="searchCategoryButton">
+                        <img src="/images/돋보기.png">
+                    </button>
+                </div> 
+    </form> <!--카테고리form-->
+        <!-------------------------------------------->
+        <div class="search_area">
+            <form id="searchForm">
+                <input type="search" name="keyword" placeholder="프로젝트 키워드 입력">
+                <span><button type="submit">검색</button></span>
+            </form>
+        </div>
+        <!-------------------------------------------->
+    </div>
+    <div class="projectList_grid">
+        <ul id="projectList">
+            <% projects.forEach(project => { %>
+                <% if (project.project_seq) { %>
+                    <li>
+                        <div class="icon_img">
+                            <img src="/images/c.png">
+                        </div>
+                        <div class="hompage_top_contents1_bold"><%= project.introduce_title %></div>
+                        <div class="hompage_top_contents1"><%= project.nickname %></div>    
+                        <div class="more"><a href="/project/<%= project.project_seq %>">들어가기</a></div>      
+                    </li>
+                <% } %>
+            <% }); %>
+        </ul>
+    </div>
+    <div>
+        <ol class="paging">
+            <li class="first"><a href="/">첫페이지</a></li>
+            <li class="prev"><a href="/">이전페이지</a></li>
+            <% if (totalPages > 1) { %>
+                <% for (let i = 1; i <= totalPages; i++) { %>
+                    <% if (i === currentPage) { %>
+                        <li><a href="/" id="page<%= i %>" data-page="<%= i %>"><%= i %></a></li>
+                    <% } else { %>
+                        <li><a href="/" id="page<%= i %>" data-page="<%= i %>"><%= i %></a></li>
+                    <% } %>
+                <% } %>
+            <% } %>
+            <li class="next"><a href="/">다음페이지</a></li>
+            <li class="last"><a href="/">마지막페이지</a></li>
+        </ol>
+    </div>
+</div>
 
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        const paginationLinks = document.querySelectorAll(".paging a");
+        paginationLinks.forEach(link => {
+            link.addEventListener("click", function(event) {
+                event.preventDefault();
+                const page = parseInt(this.getAttribute("data-page"));
+                fetchProjects(page);
+            });
+        });
 
-app.listen(port, () => {
-  console.log(`서버 실행 성공하였습니다. 접속주소 : http://localhost:${port}`)
-})
+        function fetchProjects(page) {
+            fetch('/?page=' + page)
+            .then(response => response.text())
+            .then(data => {
+                const startIndex = data.indexOf('<ul class="icons" id="projectList">');
+                const endIndex = data.indexOf('</ul>', startIndex) + 5;
+                const projectListHTML = data.substring(startIndex, endIndex);
+                document.getElementById('projectList').innerHTML = projectListHTML;
+            })
+            .catch(error => console.error('Error:', error));
+        }
+
+        //---------------------------------
+        $(document).ready(function(){
+            $('#searchForm').on('submit', function(e){
+                e.preventDefault();
+                $.ajax({
+                    type: 'POST',
+                    url: '/',
+                    data: $(this).serialize(),
+                    success: function(response){
+                        $('#projectList').html('');
+                        response.projects.forEach(function(project) {
+                            if (project.project_seq) {
+                                $('#projectList').append(`
+                                    <li>
+                                        <div class="icon_img">
+                                            <img src="/images/c.png">
+                                        </div>
+                                        <div class="hompage_top_contents1_bold">${project.introduce_title}</div>
+                                        <div class="hompage_top_contents1">${project.nickname}</div>
+                                        <div class="more"><a href="/project/${project.project_seq}">들어가기</a></div>
+                                    </li>
+                                `);
+                            }
+                        });
+                    }
+                });
+            });
+        });
+        //--------------------------------
+
+        const searchCategoryButton = document.getElementById('searchCategoryButton');
+        searchCategoryButton.addEventListener('click', function() {
+            const form = document.getElementById('searchCategoryForm');
+            const formData = new FormData(form);
+
+            fetch('/searchCategory', {
+                method: 'POST',
+                body: formData
+            })
+            .then(response => response.json())
+            .then(data => {
+                const projectList = document.getElementById('projectList');
+                projectList.innerHTML = ''; // Clear the existing projects
+
+                if (data.projects && data.projects.length > 0) {
+                    data.projects.forEach(project => {
+                        const li = document.createElement('li');
+                        li.innerHTML = `
+                            <div class="icon_img">
+                                <img src="/images/c.png">
+                            </div>
+                            <div class="hompage_top_contents1_bold">${project.introduce_title}</div>
+                            <div class="hompage_top_contents1">${project.nickname}</div>    
+                            <div class="more"><a href="/project/${project.project_seq}">들어가기</a></div>
+                        `;
+                        projectList.appendChild(li);
+                    });
+                } else {
+                    projectList.innerHTML = '<li>검색 결과가 없습니다.</li>';
+                }
+            })
+            .catch(error => console.error('Error:', error));
+        });
+    });
+</script>  
+
+<%- include('footer') %>
